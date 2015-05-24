ea6fad56f0451b87f911e476dd9b68e3c7d2fe96
kalexander12 <katielynnalexander@hotmail.com>
2015-05-23 23:09:57 -0700

    Create UserTest.java

---
+++ src/test/UserTest.java
@@ -0,0 +1,16 @@
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+
+public class UserTest {
+	
+	@Test
+	public void testCompareTo() {
+		
+		User user = new Volunteer("Jane", "Doe", "janedoe@gmail.com");
+		User otherUser = new Volunteer("John", "Doe", "johndoe@gmail.com");
+		assertEquals(user.getFirstName().compareTo(otherUser.getFirstName()), user.compareTo(otherUser));
+	}
+
+}
