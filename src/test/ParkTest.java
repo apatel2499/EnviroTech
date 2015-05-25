70eed7496c028ad6e7ff73a86d6fb7e2e9bef327
Qiyuan Zhou <zhou41uiuc@gmail.com>
2015-05-25 01:42:22 -0700

    Create ParkTest.java
    getters n setters are not tested.

---
+++ src/test/ParkTest.java
@@ -0,0 +1,51 @@
+package com.swd.project;
+
+import static org.junit.Assert.*;
+import java.util.Date;
+import java.util.TreeSet;
+
+import org.junit.Before;
+import org.junit.Test;
+
+public class ParkTest {
+	
+	private int parkId;
+
+	private String parkName;
+
+	private ParkManager parkManager;
+
+	private TreeSet<Job> jobs;
+	
+	@Before
+	public void setUp() {
+		parkId = 0;
+		parkName = "name";
+		parkManager = new ParkManager("park","manager","pm@pm.com");
+		jobs = new TreeSet<Job>();
+	}
+
+	@Test
+	public void testPark() {
+		
+		// initiate a park through constructor, so park should not be null
+		Park park = new Park(parkId, parkName, parkManager);
+		assertNotNull(park);
+		
+	}
+
+	/**
+	 * 		Note: addJob() method is called in Job class constructor. When a job is initialized,
+	 * 		it is automatically assigned to a park.
+	 */
+	@Test
+	public void testAddJob() {
+		
+		Park park = new Park(parkId, parkName, parkManager);
+		Date date = new Date();
+		Job job = new Job(0, date, park);
+
+		assertEquals(1, park.getJobs().size());
+	}
+
+}
