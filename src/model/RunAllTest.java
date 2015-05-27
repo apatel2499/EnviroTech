e5657803a4e5fb34446643dd758ac05b47194639
apatel2499 <justdoit.patel@gmail.com>
2015-05-27 15:36:06 -0700

    Run all test class created.

---
+++ src/model/RunAllTest.java
@@ -0,0 +1,18 @@
+package model;
+
+
+
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+/**
+ * Run all the JUnit tests.
+ * 
+ * @author Ankit
+ */
+@RunWith(Suite.class)
+@Suite.SuiteClasses({ AdministratorTest.class, HandlerTest.class, VolunteerTest.class, ParkManagerTest.class, 
+	JobTest.class, UserTest.class, ParkTest.class })
+
+public class RunAllTest {
+
+}
